02227b8b64f0e519bd343154965a11ae922ded38
nmor4 <nick-morin@hotmail.com>
2016-07-09 13:03:38 -0400

    Initial commit

---
+++ Assignment 4/Assignment 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Author: Nick Morin - 300654901
+ * Description: Assignment 4, Part 1: Dice Rolling Simulation
+ * Date Last Modified: 
+ */
+namespace Assignment_4
+{
+    //
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
